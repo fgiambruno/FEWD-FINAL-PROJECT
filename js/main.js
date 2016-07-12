ad5ef8d20d3e8d48a7f4966a0e2ff967b76fe489
fgiambruno <florencia.giambruno@gmail.com>
2016-07-11 20:27:51 -0500

    nav color, donate button, hr fixed, hover on buton, hover on images

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
-// $(document).ready(function(){       
-//    var scroll_start = 0;
-//    var startchange = $('.nav');
-//    var offset = startchange.offset();
-//    $(document).scroll(function() { 
-//       scroll_start = $(this).scrollTop();
-//       if(scroll_start > offset.top) {
-//           $('.nav').css('background-color', 'rgba(34,34,34,0.9)');
-//        } else {
-//           $('.nav').css('background-color', 'transparent');
-//        }
-//    });
-// });
-
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
     if (scroll >= 500) {
-        $(".nav").addClass("scrolling");
+        console.log('works')
+        $("nav").addClass("scrolling");
     } else {
-        $(".nav").removeClass("scrolling");
+        $("nav").removeClass("scrolling");
     }
 });
