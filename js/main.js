e4dd6ef348c8cf55dfe64644af9016277c1ea337
fgiambruno <florencia.giambruno@gmail.com>
2016-07-11 23:33:37 -0500

    top div 100% height, donate button,separated top image fromnav

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@ $(window).scroll(function() {
         $("nav").removeClass("scrolling");
     }
 });
+
+
+
+$(document).ready(function() {
+  var h = $( window ).height();
+  $("#topDiv").height(h);
+  $(".back").height(h);
+
+});
+
+// $(document).ready(function() {
+//   var h2 = $( window ).height();
+//   $(".about").height(h2);
+
+// });
