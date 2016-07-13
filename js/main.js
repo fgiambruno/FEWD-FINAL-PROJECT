7cc5bcb815d82710777576dae4716bbd1b32f8fc
fgiambruno <florencia.giambruno@gmail.com>
2016-07-12 21:28:15 -0500

    animation in title

--- js/main.js
+++ js/main.js
@@ -9,10 +9,12 @@ $(window).scroll(function() {
     }
 });
 
-
+var tickerIterations = 0;
+var currentTickerIteration = 0;
 
 $(document).ready(function() {
   var h = $( window ).height();
+  console
   $("#topDiv").height(h);
   $(".back").height(h);
 
@@ -21,8 +23,63 @@ $(document).ready(function() {
 $(document).ready(function() {
   var h2 = $( window ).height();
   $(".about").height(h2);
+  createTicker();
 
 });
+
+
+function createTicker(){
+  if (typeof $('#ticker-area').attr('alt') != 'undefined'){
+    tickerIterations = $('#ticker-area').attr('alt');
+  }
+  // put all list elements within #ticker-area into array
+  var tickerLIs = $("#ticker-area ul").children();
+  tickerItems = new Array();
+  tickerLIs.each(function(el) {
+    tickerItems.push( jQuery(this).html() );
+  });
+  i = 0
+  rotateTicker();
+}
+
+function rotateTicker(){
+  if( i == tickerItems.length ){
+    i = 0;
+    if( tickerIterations > 0 ){
+      console.log( "tickerIterations: " +tickerIterations );
+      currentTickerIteration++;
+      console.log( "currentTickerIteration: " + currentTickerIteration );
+      if( currentTickerIteration >= tickerIterations ){
+        console.log( "Done iterating" );
+        return false;
+      }
+    }
+  }
+  tickerText = tickerItems[i];
+  c = 0;
+  typetext();
+  setTimeout( "rotateTicker()", 5000 );
+  i++;
+}
+
+var isInTag = false;
+function typetext() { 
+  var thisChar = tickerText.substr(c, 1);
+  if( thisChar == '<' ){ isInTag = true; }
+  if( thisChar == '>' ){ isInTag = false; }
+  $('#ticker-area').html("&nbsp;" + tickerText.substr(0, c++));
+  if(c < tickerText.length+1)
+    if( isInTag ){
+      typetext();
+    }else{
+      setTimeout("typetext()", 180);
+    }
+  else {
+    c = 1;
+    tickerText = "";
+  } 
+}
+
 
 // $(.movingText).scroll(function()
 // var settings = {
